a44fffdbf1521759bb9073e4423702bc7348ceab
Milan McGraw <mcgraw@uchicago.edu>
2021-01-27 14:11:32 -0600

    Updated Changes

--- views/comments.py
+++ views/comments.py
@@ -4,28 +4,59 @@ from mongoengine import DoesNotExist, Q
 import models
 import json
 
+
 class CommentListEndpoint(Resource):
-    
+
     def get(self):
-        # TODO: implement GET endpoint
+        keyword = request.args.get('keyword')
+        if keyword:
+            # find data where *any of the fields contain the term...
+            data = models.Post.objects.filter(
+                Q(title__icontains=keyword) |
+                Q(content__icontains=keyword) |
+                Q(author__icontains=keyword)
+            )
+        else:
+            data = models.Post.objects
+
+        # formatting the output JSON
+        data = self.queryset_to_serialized_list(data)
         return Response(json.dumps([]), mimetype="application/json", status=200)
 
     def post(self):
-        # TODO: implement POST endpoint
+        body = request.get_json()
+        post = models.Post(**body).save()
+        serialized_data = {
+            'id': str(post.id),
+            'message': 'Post {0} successfully created.'.format(post.id)
+        }
         return Response(json.dumps([]), mimetype="application/json", status=201)
-        
+
+
 class CommentDetailEndpoint(Resource):
     def put(self, id):
-        # TODO: implement PUT endpoint
+        # body = request.get_json()
+        post = models.Post.objects.get(id=id)
+        request_data = request.get_json()
+        post.title = request_data.get('title')
+        post.content = request_data.get('content')
+        post.author = request_data.get('author')
+        post.save()
+        print(post.to_json())
         return Response(json.dumps([]), mimetype="application/json", status=200)
-    
+
     def delete(self, id):
-        # TODO: implement DELETE endpoint
+        post = models.Post.objects.get(id=id)
+        post.delete()
+        serialized_data = {
+            'message': 'Post {0} successfully deleted.'.format(id)
+        }
         return Response(json.dumps([]), mimetype="application/json", status=200)
 
     def get(self, id):
-        # TODO: implement GET endpoint
+        post = models.Post.objects.get(id=id)
         return Response(json.dumps([]), mimetype="application/json", status=200)
+
 
 def initialize_routes(api):
     api.add_resource(CommentListEndpoint, '/api/comments', '/api/comments/')
